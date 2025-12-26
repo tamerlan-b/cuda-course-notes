317eb2580bece64d5ad966351ce11a1031161177
Tamerlan <>
2025-12-26 10:29:03 +0300

    feat: Обновлен код для профилирования

--- 08_ncu_profiling/test.cu
+++ 08_ncu_profiling/test.cu
@@ -1,30 +1,57 @@
 #include "cuda_runtime.h"
 #include "device_launch_parameters.h"
+#include <cuda.h>
 
 #include <stdio.h>
 
-__global__ void sum(int* a, int* b, int* c)
+
+inline void gpuAssert(cudaError_t err_code, const char* file, int line, bool abort=true)
 {
-    int i = threadIdx.x;
+    if(err_code != cudaSuccess)
+    {
+        fprintf(stderr, "Cuda error: %s %s line %d\n", cudaGetErrorString(err_code), file, line);
+        if(abort){exit(err_code);}
+    }
+}
+// Error checking macro
+#define cudaCheckError(res){ gpuAssert(res, __FILE__, __LINE__); }
+
+inline void gpuKernelAssert(const char* file, int line, bool abort=true)
+{
+    cudaError_t err_code = cudaGetLastError();
+    if(err_code != cudaSuccess)
+    {
+        fprintf(stderr, "Cuda kernel error: %s %s line %d\n", cudaGetErrorString(err_code), file, line);
+        if(abort){exit(err_code);}
+    }
+}
+#define cudaKernelCheckError(){ gpuKernelAssert(__FILE__, __LINE__); }
+
+
+__global__ void sum(int* a, int* b, int* c, long n)
+{
+    int i = blockIdx.x * blockDim.x + threadIdx.x;
     c[i] = a[i] + b[i];
 }
 
 int main()
 {
-    const int size = 1024;
+    const long size = 1024LL * 1024 * 32;
 
+    // cudaError_t err;
+    
+    // Allocate memory on GPU
+    int* a_d, *b_d, *c_d;
+    cudaCheckError(cudaMalloc((void**)&a_d, sizeof(int) * size))    // Check synchronous error
+    cudaCheckError(cudaMalloc((void**)&b_d, sizeof(int) * size))    // Check synchronous error
+    cudaCheckError(cudaMalloc((void**)&c_d, sizeof(int) * size))    // Check synchronous error
+    // kernal_name<<<num_of_blocks, num_of_threads_per_block>>>();
+    // cudaDeviceSynchronize();
+    
     // Allocate memory on CPU
     int* a = (int*)malloc(sizeof(int) * size);
     int* b = (int*)malloc(sizeof(int) * size);
     int* c = (int*)malloc(sizeof(int) * size);
-
-    // Allocate memory on GPU
-    int* a_d, *b_d, *c_d;
-    cudaMalloc((void**)&a_d, sizeof(int) * size);
-    cudaMalloc((void**)&b_d, sizeof(int) * size);
-    cudaMalloc((void**)&c_d, sizeof(int) * size);
-    // kernal_name<<<num_of_blocks, num_of_threads_per_block>>>();
-    // cudaDeviceSynchronize();
 
     // Fill array with values
     for (int i = 0; i < size; ++i)
@@ -37,12 +64,27 @@ int main()
     cudaMemcpy(a_d, a, sizeof(int) * size, cudaMemcpyHostToDevice);
     cudaMemcpy(b_d, b, sizeof(int) * size, cudaMemcpyHostToDevice);
 
+    const int grid_num = 64;
+    const int block_dim = 32;
+    cudaEvent_t start, stop;
+    cudaEventCreate(&start);
+    cudaEventCreate(&stop);
     // Calculate sum
-    sum<<<1, 1024>>>(a_d ,b_d, c_d);
+    cudaEventRecord(start);
+    
+    sum<<<grid_num, block_dim>>>(a_d ,b_d, c_d, size);
+    cudaKernelCheckError()  // Check asynchronous error
+
     cudaDeviceSynchronize();
+    cudaEventRecord(stop);
+    cudaEventSynchronize(stop);
+    float milliseconds = 0.F;
+    cudaEventElapsedTime(&milliseconds, start, stop);
     
     // Copy result from GPU tp CPU
     cudaMemcpy(c, c_d, sizeof(int) * size, cudaMemcpyDeviceToHost);
+
+    printf("Execution time sum<<<%d,%d>>>(): %f ms\n", grid_num, block_dim, milliseconds);
 
     // Print result
     // printf("Execution completed\n");
@@ -51,6 +93,8 @@ int main()
     //     printf("%d + %d = %d\n", a[i], b[i], c[i]);
     // }
 
+    cudaEventDestroy(start);
+    cudaEventDestroy(stop);
 
     // Free memory on GPU
     cudaFree(a_d);
